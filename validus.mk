bd804726dac1d4f432b18ae9874b9494ad1c0481
RinkleMcBally <rinklemcbally22@gmail.com>
2015-06-05 20:18:05 -0500

    Update and rename cm.mk to validus.mk

--- validus.mk
+++ validus.mk
@@ -1,10 +1,10 @@
 $(call inherit-product, device/samsung/d2att/full_d2att.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/validus/config/nfc_enhanced.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/validus/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=d2uc \
@@ -12,6 +12,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT="samsung/d2uc/d2att:4.1.2/JZO54K/I747UCDMG2:user/release-keys" \
     PRIVATE_BUILD_DESC="d2uc-user 4.1.2 JZO54K I747UCDMG2 release-keys"
 
-PRODUCT_NAME := cm_d2att
+PRODUCT_NAME := validus_d2att
 PRODUCT_DEVICE := d2att
 
